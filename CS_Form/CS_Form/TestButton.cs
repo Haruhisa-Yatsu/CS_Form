a06b4639604ae610cfd98f776bebbe23c5a9b205
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-11-01 14:07:04 +0900

    ボタンにテキスト表示　ボタンを複数個作成　ボタン位置の指定

--- CS_Form/CS_Form/TestButton.cs
+++ CS_Form/CS_Form/TestButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,17 @@ namespace CS_Form
         /// コンストラクタ
         /// クラスを生成したときに呼び出される
         /// </summary>
-        public TestButton()
+        public TestButton(int x, int y)
         {
             // ClickイベントにOnClick関数を登録
             // ボタンをクリックした時に登録した関数を実行します。
             Click += OnClick;
+
+            // ボタン内に文字を表示させる
+            Text = "ボタン";
+
+            Location = new Point(x,y);
+ 
         }
 
         /// <summary>
