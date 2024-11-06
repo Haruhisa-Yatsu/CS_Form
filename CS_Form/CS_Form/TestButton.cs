dae08a5b59e7ebef34195998a5b983cfc76e4a11
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-11-06 11:35:23 +0900

    ラベルの文字列変更処理作成

--- CS_Form/CS_Form/TestButton.cs
+++ CS_Form/CS_Form/TestButton.cs
@@ -12,12 +12,17 @@ namespace CS_Form
 {
     class TestButton : Button
     {
+        Form1 _form1;
+
         /// <summary>
         /// コンストラクタ
         /// クラスを生成したときに呼び出される
         /// </summary>
-        public TestButton(int id , int x, int y, int width, int height)
+        public TestButton(Form1 form1, int id , int x, int y, int width, int height)
         {
+
+            _form1 = form1;
+
             // ClickイベントにOnClick関数を登録
             // ボタンをクリックした時に登録した関数を実行します。
             Click += OnClick;
@@ -40,7 +45,7 @@ namespace CS_Form
         /// <param name="s"></param>
         public void OnClick(object sender, EventArgs s)
         {
-            MessageBox.Show(Text);
+            _form1.LabelTextUpdate(Text);
         }
     }
 }
